6878ccbef3e8e16c92c102ba2c46724b56cc79fa
Addidnal <Assisnal@gmail.com>
2020-09-29 16:34:01 +0200

    fixing original content trailers

---
+++ precache-manifest.7f10a01b8b38e125061944cf0484cf83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98e8e5f57def6ae6f916c8b95383d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b4fb9cdea8c3dbd164a",
+    "url": "/static/css/main.18145dc8.chunk.css"
+  },
+  {
+    "revision": "fe214af7445f853b205c",
+    "url": "/static/js/2.947604b9.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.947604b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4fb9cdea8c3dbd164a",
+    "url": "/static/js/main.9c36e027.chunk.js"
+  },
+  {
+    "revision": "1ce858f70d189b292625",
+    "url": "/static/js/runtime-main.f373eef6.js"
+  }
+]);
